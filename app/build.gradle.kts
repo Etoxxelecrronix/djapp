be65c75c34c1da7af62e3f0c3e7b997150fc18e9
Etoxxelecrronix <etoxxelecrronix@users.noreply.github.com>
2026-07-29 18:41:37 +0000

    Remove obsolete proguard-rules.pro reference from release buildType (isMinifyEnabled=false)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,10 +22,6 @@ android {
         }
         release {
             isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
         }
     }
 
